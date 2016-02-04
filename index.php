4fa99d5aaf5d2ee0177daca3f22f37727be7e485
Mohammed Jafar <jafartke@gmail.com>
2016-02-04 16:57:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+
+
 echo "testing php here";
+
+$link = mysql_connect("172.30.153.208", "tgt_admin", "qazwsx$#21");
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+    
+    
 ?>
